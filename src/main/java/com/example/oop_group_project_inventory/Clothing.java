177b9ee2c1117d661e67c16293999772ca498a40
002sky <87744714+002sky@users.noreply.github.com>
2023-07-12 16:12:43 +0800

    update on the tableview for three type of product

--- src/main/java/com/example/oop_group_project_inventory/Clothing.java
+++ src/main/java/com/example/oop_group_project_inventory/Clothing.java
@@ -1,5 +1,9 @@
 package com.example.oop_group_project_inventory;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Clothing extends Product {
 
     private String clothingType;
@@ -48,5 +52,45 @@ public class Clothing extends Product {
 
     public void setMaterial(String material) {
         this.material = material;
+    }
+
+    public ArrayList<Clothing> loadFromDatabase() {
+        ArrayList<Clothing> ClothingList = new ArrayList<>();
+        String url = "jdbc:ucanaccess://src/main/resources/Inventory.accdb";
+
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+            String sql = "SELECT * FROM Clothing LEFT JOIN Product ON Clothing.productID = Product.productID";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                String id = result.getString("productID");
+                String name = result.getString("Product.productName");
+                double unitPrice = result.getDouble("Product.unitPrice");
+                double sellingPrice = result.getDouble("Product.sellingPrice");
+                String brand = result.getString("Product.productBrand");
+                boolean status = result.getBoolean("Product.productStatus");
+                String type = result.getString("clothingType");
+                char size = result.getString("clothingSize").charAt(0);
+                String color = result.getString("color");
+                String material = result.getString("material");
+
+                Clothing a = new Clothing(id,name,unitPrice,sellingPrice,brand,status,type,size,color,material);
+
+                ClothingList.add(a);
+
+                System.out.println(a.getProductID() + " " + a.getProductName() + " " + a.getSellingPrice());
+            }
+
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return ClothingList;
+
     }
 }
